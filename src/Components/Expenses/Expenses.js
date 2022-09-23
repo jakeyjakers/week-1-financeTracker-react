3cbc1c3c02a6a55a1790c17f24e4980eecb06a28
Jacob Eichler <jeichler7@gmail.com>
2022-09-23 17:35:31 -0600

    updates

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -1,45 +1,33 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
-import './Expenses.css'
+import ExpensesList from "./ExpensesList";
+import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
-
-  const [filterdYear, setFilteredYear] = useState('2020')
+  const [filterdYear, setFilteredYear] = useState("2020");
 
   const filteredChangeHandler = (selectedYear) => {
-    setFilteredYear(selectedYear)
-    console.log('expenses.js')
-    console.log(selectedYear)
-  }
+    setFilteredYear(selectedYear);
+    console.log("expenses.js");
+    console.log(selectedYear);
+  };
+  
+  const filteredExpenses = props.item.filter((expense) => {
+    return expense.date.getFullYear().toString() === filterdYear;
+  });
+
+  
 
   return (
     <div>
-      
-    <Card className="expenses">
-    <ExpensesFilter onChangeFilter={filteredChangeHandler} selected={filterdYear}/>
-      <ExpenseItem
-        title={props.item[0].title}
-        amount={props.item[0].amount}
-        date={props.item[0].date}
-      />
-      <ExpenseItem
-        title={props.item[1].title}
-        amount={props.item[1].amount}
-        date={props.item[1].date}
-      />
-      <ExpenseItem
-        title={props.item[2].title}
-        amount={props.item[2].amount}
-        date={props.item[2].date}
-      />
-      <ExpenseItem
-        title={props.item[3].title}
-        amount={props.item[3].amount}
-        date={props.item[3].date}
-      />
-    </Card>
+      <Card className="expenses">
+        <ExpensesFilter
+          onChangeFilter={filteredChangeHandler}
+          selected={filterdYear}
+        />
+        <ExpensesList item={filteredExpenses}/>
+      </Card>
     </div>
   );
 };
--- src/Components/NewExpense/NewExpense.js
+++ src/Components/NewExpense/NewExpense.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import './NewExpense.css'
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false)
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
@@ -11,8 +12,12 @@ const NewExpense = (props) => {
         props.onAddExpense(expenseData)
     }
 
+    const startEditingHandler = () => {
+      setIsEditing(true)
+    }
+
   return (
-    <div className="new-expense">
+    <div onClick={startEditingHandler} className="new-expense">
       <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
     </div>
   );
